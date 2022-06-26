721dc445f54e631a88cbfea4b77604d8b51953fe
Mohit Singh <mohits2125@gmail.com>
2022-06-26 15:29:37 +0530

    pic added

--- js/script.js
+++ js/script.js
@@ -6,20 +6,7 @@
       $('.sakura-falling').sakura();
 })(jQuery);
 
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
+
 
  document.getElementById("my_audio").play();
 // $(document).on('click', function(){
@@ -40,7 +27,7 @@ function playAudio() {
     document.getElementById("my_audio").pause();
   } 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 28, 2022 00:00:00").getTime();
+var countDownDate = new Date("Feb 15, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
